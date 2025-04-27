51353cf48a10fab765ab0a869c27377b8f478b69
Chase Bryan <chasekbryan@icloud.com>
2025-04-27 01:29:44 -0500

    modified:   bookmarks.txt

---
+++ first_word/target/debug/deps/first_word-81ac036e53531dca.d
@@ -0,0 +1,5 @@
+/Users/chbryan/projects/first_word/target/debug/deps/first_word-81ac036e53531dca: src/main.rs
+
+/Users/chbryan/projects/first_word/target/debug/deps/first_word-81ac036e53531dca.d: src/main.rs
+
+src/main.rs:
---
+++ first_word/target/debug/first_word.d
@@ -0,0 +1,1 @@
+/Users/chbryan/projects/first_word/target/debug/first_word: /Users/chbryan/projects/first_word/src/main.rs
